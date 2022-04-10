4b617def8761b74c8514a34ba4de17a702706b60
Dusu <noreply@github.com>
2022-04-10 14:57:06 +0800

    fix bash installation script

--- install.sh
+++ install.sh
@@ -2,25 +2,25 @@
 
 # Vim
 
-runtime_path="~\.vim_runtime"
-vimrc_source="${runtime_path}\vimrc"
-vimrc_destination="~\.vimrc"
-gvimrc_destination="~\.gvimrc"
+runtime_path=~\.vim_runtime
+vimrc_source=${runtime_path}\vimrc
+vimrc_destination=~\.vimrc
+gvimrc_destination=~\.gvimrc
 
 ln -s $vimrc_source $vimrc_destination
 touch $gvimrc_destination
 
 # Neovim
 
-nvim_config_dir="~/.config/nvim"
+nvim_config_dir=~/.config/nvim
 
 if [[ ! -d $nvim_config_dir ]]; then
     mkdir -p $nvim_config_dir
 fi
 
-nvimrc_destination="${nvim_config_dir}\init.vim"
-gnvimrc_source="${runtime_path}\ginit.vim"
-gnvimrc_destination="${nvim_config_dir}\ginit.vim"
+nvimrc_destination=${nvim_config_dir}\init.vim
+gnvimrc_source=${runtime_path}\ginit.vim
+gnvimrc_destination=${nvim_config_dir}\ginit.vim
 
 ln -s $vimrc_source $nvimrc_destination
 ln -s $gnvimrc_source $gnvimrc_destination
