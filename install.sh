f116ed5c6c96fd67a319c4be26ccdf0a4db41e30
dodowhat <noreply@github.com>
2024-04-14 10:31:23 +0800

    update installation script

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
 sdir=$(dirname $(realpath ${BASH_SOURCE[0]}))
-nvimdir=~/.config/nvim
+svimrc=$sdir/vimrc
+dvimrc=~/.vimrc
+nvimconfd=~/.config/nvim
+rootconfd=/root/.config
 
-ln -sf $sdir/vimrc ~/.vimrc
+ln -sf $svimrc $dvimrc
+mkdir -p $nvimconfd
+ln -sf $svimrc $nvimconfd/init.vim
 
-mkdir -p $nvimdir
-ln -sf $sdir/vimrc $nvimdir/init.vim
+if [[ $1 == "sudo" ]]; then
+    sudo ln -sf $dvimrc /root
+    sudo mkdir -p $rootconfd
+    sudo ln -sf $nvimconfd $rootconfd
+fi
 
