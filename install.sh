8f7fb21fd52ca0855761b2f24131b960318bf173
dodowhat <noreply@github.com>
2024-05-09 19:31:58 +0800

    fix installation script

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ dvimrc=~/.vimrc
 nvimconfd=~/.config/nvim
 rootconfd=/root/.config
 
-if [[ ${EUID} != 0 ]]; then
+ln -sf $svimrc $dvimrc
+mkdir -p $nvimconfd
+ln -sf $svimrc $nvimconfd/init.vim
+
+sudo -v > /dev/null 2>&1
+if [[ $? == 0 && ${EUID} != 0 ]]; then
     sudo ln -sf $dvimrc /root
     sudo mkdir -p $rootconfd
     sudo ln -sf $nvimconfd $rootconfd
-else
-    ln -sf $svimrc $dvimrc
-    mkdir -p $nvimconfd
-    ln -sf $svimrc $nvimconfd/init.vim
 fi
-
