3d0e8cb2edb624303fb6dc7542a46e2e56c411f6
dodowhat <noreply@github.com>
2025-05-03 14:14:17 +0800

    add telescope.nvim

--- README.md
+++ README.md
@@ -20,3 +20,8 @@ plugin manager: [tpope/vim-pathogen](https://github.com/tpope/vim-pathogen)
 
 file explorer: [stevearc/oil.nvim](https://github.com/stevearc/oil.nvim)
 
+fuzzy finder:
+[telescope.nvim](https://github.com/nvim-telescope/telescope.nvim)
+(deps: [nvim-lua/plenary.nvim](https://github.com/nvim-lua/plenary.nvim),
+[BurntSushi/ripgrep](https://github.com/BurntSushi/ripgrep))
+
--- init.lua
+++ init.lua
@@ -1,11 +1,18 @@
---
-
+-- load basic vimrc
 local script_path = debug.getinfo(1, "S").source:match("^@(.+)$")
 local script_dir = script_path:match("^(.*[\\/])")
-
 vim.cmd('source ' .. script_dir .. '/vimrc')
 
+-- pathogen
 vim.fn['pathogen#infect']('plugins/{}')
 
+-- oil.vim
 require("oil").setup()
+
+-- telescope.nvim
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
